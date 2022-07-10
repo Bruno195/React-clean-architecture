4e3ed7dea455de78970b76cf0fe601d0b9e90944
Bruno195 <brunomonteiro195@gmail.com>
2022-07-10 20:29:52 -0300

    feat: ensure RemoteAuthentication calss HttpPost with correct body

--- src/domain/usecases/Authentication.ts
+++ src/domain/usecases/Authentication.ts
@@ -1,8 +1,8 @@
 import { AccountModel } from '../models/account-model'
 
-type AuthenticationParams = {
+export type AuthenticationParams = {
   email: string
-  assword: string
+  password: string
 }
 export interface Authentication {
   auth: (params: AuthenticationParams) => Promise<AccountModel> // implementar esste modulo de forma assincrona
